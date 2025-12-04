bb44d1a7a16400e51a38a63662ee38ef7b4cfe27
Will Fenton <wfenton@protonmail.com>
2025-12-03 22:39:39 -0700

    Day 4

--- src/main/kotlin/aoc2025/Day04.kt
+++ src/main/kotlin/aoc2025/Day04.kt
@@ -1,5 +1,7 @@
 package aoc2025
 
+import Grid
+import Position
 import Solver
 import downloadInput
 import inputFile
@@ -7,26 +9,72 @@ import java.nio.file.Path
 
 // https://adventofcode.com/2025/day/4
 
+enum class Tile {
+    EMPTY,
+    PAPER
+}
+
 class Day04(file: Path) : Solver {
     private val input = InputParser.parseLines(file.toString())
 
-    override fun solvePart1(): String {
-        return "1"
+    private val grid = Grid<Tile>()
+
+    // number of papers removed at each step
+    private val papersRemoved = mutableListOf<Int>()
+
+    init {
+        for (y in input.indices) {
+            val line = input[y]
+            for (x in line.indices) {
+                val tile = when (input[y][x]) {
+                    '.' -> Tile.EMPTY
+                    '@' -> Tile.PAPER
+                    else -> throw IllegalArgumentException()
+                }
+                grid[Position(x, y)] = tile
+            }
+        }
+        grid.init()
+
+        do {
+            papersRemoved.add(removePaper())
+        } while (papersRemoved.last() > 0)
     }
 
-    override fun solvePart2(): String {
-        return "1"
+    private fun removePaper(): Int {
+        val paperCells = grid.cells.values.filter { it.data == Tile.PAPER }
+        val cellsToRemove = paperCells.filter { gridCell ->
+            val neighbors = gridCell.neighbors(directions = Direction.ALL)
+            neighbors.count { it.data == Tile.PAPER } < 4
+        }
+        cellsToRemove.forEach { gridCell ->
+            gridCell.data = Tile.EMPTY
+        }
+        return cellsToRemove.size
     }
+
+    override fun solvePart1(): String = papersRemoved.first().toString()
+
+    override fun solvePart2(): String = papersRemoved.sum().toString()
 }
 
 fun main() {
     Day04(
         inputFile(
             """
-            input
+            ..@@.@@@@.
+            @@@.@.@.@@
+            @@@@@.@.@@
+            @.@@@@..@.
+            @@.@@@@.@@
+            .@@@@@@@.@
+            .@.@.@.@@@
+            @.@@@.@@@@
+            .@@@@@@@@.
+            @.@.@@@.@.
             """.trimIndent()
         )
-    ).run(part1ExpectedSolution = "", part2ExpectedSolution = null)
+    ).run(part1ExpectedSolution = "13", part2ExpectedSolution = "43")
 
-    Day04(downloadInput(2025, 4)).run(part1ExpectedSolution = null, part2ExpectedSolution = null)
+    Day04(downloadInput(2025, 4)).run(part1ExpectedSolution = "1516", part2ExpectedSolution = "9122")
 }
